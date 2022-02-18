10346ec81ccb91983f12e28dd2a948cefdbd4059
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-02-18 19:14:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+pacman -S --noconfirm --needed archlinux-keyring
+pacman -S --needed --noconfirm archiso mkinitcpio-archiso
 mkarchiso -v -w ./work -o ./out ./archiso/
 echo "ISO Built in ./out folder!"
