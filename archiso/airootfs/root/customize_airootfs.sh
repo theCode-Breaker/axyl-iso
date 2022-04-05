9aa59ecc87b6eb7effaa7ba9a4fb4d1e6db6b114
Kungger-git <matthewriel63@gmail.com>
2022-04-05 18:22:43 +0800

    [Update]: Append more configuration files/folders to `rootdots` & `rootcfg` array.

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -6,10 +6,10 @@ skeldir="/etc/skel"
 rootdir="/root/.config"
 if [[ ! -d "$rootdir" ]]; then mkdir -p "$rootdir"; fi
 
-declare -a rootdots=('.local' '.gtkrc-2.0' '.vim_runtime' '.vimrc')
+declare -a rootdots=('.local' '.gtkrc-2.0' '.vim_runtime' '.vimrc' '.zshrc' '.zshrc-personal' '.dmrc' '.hushlogin')
 for dfile in "${rootdots[@]}"; do if [[ -e "$skeldir/$dfile" ]]; then cp -rf "$skeldir"/"$dfile" /root; fi done
 
-declare -a rootconfig=(i3 geany gtk-3.0 Kvantum neofetch nvim qt5ct ranger Thunar fish)
+declare -a rootconfig=('i3' 'geany' 'gtk-2.0' 'gtk-3.0' 'Kvantum' 'neofetch' 'nvim' 'qt5ct' 'ranger' 'Thunar' 'xfce4' 'conkeww' 'pulse' 'sxhkd' 'cava')
 for cfg in "${rootconfig[@]}"; do if [[ -e "$skeldir/.config/$cfg" ]]; then cp -rf "$skeldir"/.config/"$cfg" "$rootdir"; fi done
 
 # Replace Default Mouse Cursor Theme
